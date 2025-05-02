fdee1071a8af8b90cdc28191961329f02b13560a
Wululapuda <lk500169k@163.com>
2025-05-02 15:22:58 +0800

    Add files via upload

---
+++ RegisterHandler.py
@@ -0,0 +1,13 @@
+class RegisterHandler:
+    class ressources:
+        _models = []
+        _character = []
+        def get(self,name):
+            pass
+        def getAll(self):
+            pass
+    class Register:
+        def __init__(self,name:str):
+            self.name = name
+        def models(self):
+            RegisterHandler.ressources._models.
---
+++ main.py
@@ -0,0 +1,6 @@
+from direct.showbase.ShowBase import ShowBase
+screen = ShowBase()
+screen.run()
+#启动入口
+if __name__ == "__main__":
+    screen.run()
